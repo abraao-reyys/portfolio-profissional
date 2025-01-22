82d9cda5dd584239a8815c10bc10d313b957d320
abraao-reyys <146043864+abraao-reyys@users.noreply.github.com>
2025-01-22 14:54:47 -0300

    Detail Upd

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const cv_button = window.document.getElementById('cv-button');
 const contact_button = window.document.getElementById('contact-button');
+const menu_button = window.document.getElementById('menu-button');
 
 cv_button.addEventListener('click', aviso);
 contact_button.addEventListener('click', aviso);
+menu_button.addEventListener('click', aviso);
 
 function aviso() {
-    window.alert('Calma, meu chapa. Sua hora vai chegar.')
+    window.alert('Mas já quer me usar? voo nadaa')
 }
