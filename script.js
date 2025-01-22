f14640d0c79af32703a132ad0cf69207829e3175
abraao-reyys <146043864+abraao-reyys@users.noreply.github.com>
2025-01-22 18:45:52 -0300

    Projects Section Upd

--- script.js
+++ script.js
@@ -9,3 +9,38 @@ menu_button.addEventListener('click', aviso);
 function aviso() {
     window.alert('Mas já quer me usar? voo nadaa')
 }
+
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 3, // Número de slides visíveis ao mesmo tempo
+    centeredSlides: true, // Centraliza o slide ativo
+    loop: true, // Loop infinito
+    spaceBetween: 50, // Espaço entre slides
+    effect: 'coverflow', // Efeito de rotação
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 100,
+        modifier: 2,
+        slideShadows: false,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    });
+    swiper.on('slideChange', () => {
+        const activeIndex = swiper.realIndex; // Índice do slide central ativo (realIndex ignora o loop)
+        
+        // Opcional: adiciona uma classe "ativo" ao slide central
+        swiper.slides.forEach((slide, index) => {
+            if (index === swiper.activeIndex) {
+                slide.classList.add('ativo');
+            } else {
+                slide.classList.remove('ativo');
+            }
+        });
+    });
