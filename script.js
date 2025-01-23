7b961c3ed5eb2600ff7eeeba9378f44650cc4120
abraao-reyys <146043864+abraao-reyys@users.noreply.github.com>
2025-01-23 18:44:58 -0300

    Details Upd

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-const cv_button = window.document.getElementById('cv-button');
-const contact_button = window.document.getElementById('contact-button');
-const menu_button = window.document.getElementById('menu-button');
-
-cv_button.addEventListener('click', aviso);
-contact_button.addEventListener('click', aviso);
-menu_button.addEventListener('click', aviso);
-
-function aviso() {
-    window.alert('Mas já quer me usar? voo nadaa')
-}
-
 const swiper = new Swiper('.swiper', {
     slidesPerView: 3, // Número de slides visíveis ao mesmo tempo
     centeredSlides: true, // Centraliza o slide ativo
